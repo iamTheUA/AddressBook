ba3f3589805018f248d82b1eec3bbbe0b7974862
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-04 18:30:21 +0530

    adding: Class Person

--- AddresBookSystem.java
+++ AddresBookSystem.java
@@ -1,10 +1,21 @@
 package Day9;
 
 public class AddresBookSystem {
+	public int name;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Welcome to Address Book Program ");
 	}
+}
 
+class Person {
+	String fName;
+	String lName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	long pNumber;
+	String email;
 }
